45c633bd2044f903e008b72d738fdb88268561a1
lupintus71 <89031186+lupintus71@users.noreply.github.com>
2022-03-04 21:45:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     btnNo: 'Có Cái đầu buồi',
     question:'Trên thế giới hơn 7 tỉ người mà sao các đệ lại làm đệ tao',
     btnReply: 'Gửi cho đại ca',
-    reply: 'Làm đệ',
+    reply: 'Làm đệ đại ca Tú',
     mess: 'Đại ca biết mà',
     messDesc: 'Tối nay 7h đi cháy phố nhé.',
     btnAccept: 'OK Đại ca Tú',
